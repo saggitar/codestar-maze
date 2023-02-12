26a95b789c2917e435dafc9d09188c37a61ce935
Maximilian Schmidt <landlaeufer@gmail.com>
2023-02-12 17:17:25 +0100

    Updated help hints

--- README.rst
+++ README.rst
@@ -12,9 +12,25 @@ Installing the package also installs the `maze-gen` entry point.
     usage: maze-gen [-h] [--file FILE] {growing_tree} ...
 
     positional arguments:
-      {growing_tree}  sub-command help
+      {growing_tree}  uses the growing tree algorithm to generate the maze
 
     options:
       -h, --help      show this help message and exit
-      --file FILE
+      --file FILE     file to redirect output
 
+Currently only the `growing_tree` algorithm is supported. The appropriate
+subcommand takes the following arguments:
+
+::
+
+    usage: maze-gen growing_tree [-h] [--choice {random,last,mixed}] [--t T] w h
+
+    positional arguments:
+      w                     Width of maze
+      h                     Height of maze
+
+    options:
+      -h, --help            show this help message and exit
+      --choice {random,last,mixed}
+                            which method to choose next cell
+      --t T                 Mixes between 'random' and 'last' choice. No effect for 'choice' other than 'mixed'
--- src/codestare/mazes/main.py
+++ src/codestare/mazes/main.py
@@ -15,17 +15,29 @@ def parse_args() -> argparse.Namespace:
     """
     Returns: namespace of arguments
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='maze-gen')
     parser.add_argument(
         "--file",
-        default=''
+        default='',
+        help='file to redirect output'
     )
-    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers = parser.add_subparsers(help='uses the growing tree algorithm to generate the maze')
     parser_growing_tree = subparsers.add_parser('growing_tree')
-    parser_growing_tree.add_argument('w', type=int)
-    parser_growing_tree.add_argument('h', type=int)
-    parser_growing_tree.add_argument('--choice', type=str, default='default_choice')
-    parser_growing_tree.add_argument('--t', type=float, default=0)
+    parser_growing_tree.add_argument('w', type=int, help='Width of maze')
+    parser_growing_tree.add_argument('h', type=int, help='Height of maze')
+    parser_growing_tree.add_argument(
+        '--choice',
+        type=str,
+        default='mixed',
+        help='which method to choose next cell',
+        choices=['random', 'last', 'mixed']
+    )
+    parser_growing_tree.add_argument(
+        '--t',
+        type=float,
+        default=0,
+        help="Mixes between 'random' and 'last' choice. No effect for 'choice' other than 'mixed'"
+    )
     parser_growing_tree.set_defaults(func=growing_tree)
 
     if len(sys.argv) == 1:
